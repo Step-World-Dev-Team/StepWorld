33ca4ebe45bb975a869c8bb5784ac56eb94c7d0d
Isai Soria <isaisoria@gmail.com>
2025-10-02 20:51:50 -0700

    Create XCode project

---
+++ StepWorld/StepWorld/StepWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepWorldApp.swift
+//  StepWorld
+//
+//  Created by Isai soria on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StepWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
