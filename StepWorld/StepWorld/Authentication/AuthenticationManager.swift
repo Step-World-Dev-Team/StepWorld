5eb27546c952d94ca311bf3f1ef206e3f0c9f64f
Isai Soria <isaisoria@gmail.com>
2025-10-06 23:11:22 -0700

    feat: add user authentication

---
+++ StepWorld/StepWorld/Authentication/AuthenticationManager.swift
@@ -0,0 +1,53 @@
+//
+//  Authentication Manager.swift
+//  StepWorld
+//
+//  Created by Isai soria on 10/6/25.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoURL: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoURL = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    private init() {}
+    
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return AuthDataResultModel(user: user)
+    }
+}
+
+// MARK: SIGN IN EMAIL
+extension AuthenticationManager {
+    
+    @discardableResult
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+}
--- StepWorld/StepWorld/ContentView.swift
+++ StepWorld/StepWorld/ContentView.swift
@@ -6,17 +6,58 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = SignInEmailViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("StepWorld is Live!")
+            
+            Spacer()
+            
+            // following two buttons are for testing
+            // assign the email and password that will be used
+            Button("Set Email") {
+                viewModel.email = "fakeUser@testing.com"
+            }
+            
+            Button("Set Password") {
+                viewModel.password = "password"
+            }
+            
+            Spacer()
+            
+            // button to sign in user
+            Button{
+                Task {
+                    do {
+                        try await viewModel.SignIn()
+                        // typically you should be sending them to another view here
+                        return
+                    } catch {
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
+    
+
 }
 
 #Preview {
