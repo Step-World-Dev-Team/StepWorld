c4ec61436dba5ba898d23d2a0838773ba61b5140
Isai Soria <isaisoria@gmail.com>
2025-10-24 12:42:07 -0700

    refactor: reduce clutter of UserManager

--- StepWorld/StepWorld/Firestore/UserManager.swift
+++ StepWorld/StepWorld/Firestore/UserManager.swift
@@ -9,6 +9,7 @@ import Foundation
 import FirebaseFirestore
 import FirebaseSharedSwift
 
+// MARK: DBUser Model
 struct DBUser: Codable {
     // all variables that are in DBUser
     // more varaiables can be added here,
@@ -18,59 +19,16 @@ struct DBUser: Codable {
     let photoUrl: String?
     let name: String?
     
-    // pulls/matches variables from authentication manager
-    init(auth: AuthDataResultModel) {
-        self.userId = auth.uid
-        self.email = auth.email
-        self.photoUrl = auth.photoURL
-        self.name = auth.name
-    }
-    
-    // initializing all variables
-    init (
-        userId: String,
-        email: String? = nil ,
-        photoUrl: String? = nil,
-        name: String? = nil
-    ) {
-        self.userId = userId
-        self.email = email
-        self.photoUrl = photoUrl
-        self.name = name
-    }
-    
-    // keys that the encoder and decoder can use
-    // to know how it should format the data
-    // variable on the left and DB value format on right
     enum CodingKeys: String, CodingKey {
        case userId = "user_id"
         case email = "email"
         case photoUrl = "photo_url"
         case name = "name"
     }
-
-    // decoder
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.userId = try container.decode(String.self, forKey: .userId)
-        self.email = try container.decodeIfPresent(String.self, forKey: .email)
-        self.photoUrl = try container.decodeIfPresent(String.self, forKey: .photoUrl)
-        self.name = try container.decodeIfPresent(String.self, forKey: .name)
-        
-    }
-    
-    // encoder
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(userId, forKey: .userId)
-        try container.encodeIfPresent(email, forKey: .email)
-        try container.encodeIfPresent(photoUrl, forKey: .photoUrl)
-        try container.encodeIfPresent(name, forKey: .name)
-    }
     
 }
 
-
+// MARK: User Manager
 final class UserManager {
     
     static let shared = UserManager()
@@ -78,7 +36,7 @@ final class UserManager {
     
     // pulls the collection called Users
     // This call can be replicated for whichever collection that is needed
-    // "Colleciton" representing a folder in the DB
+    // "Collection" representing a folder in the DB
     private let userCollection = Firestore.firestore().collection("Users")
     
     // pulls the individual document from the User folder
@@ -88,13 +46,11 @@ final class UserManager {
     
     private let encoder: Firestore.Encoder = {
         var encoder = Firestore.Encoder()
-        //encoder.keyEncodingStrategy = .convertToSnakeCase
         return encoder
     }()
     
     private let decoder: Firestore.Decoder = {
         var decoder = Firestore.Decoder()
-        //decoder.keyDecodingStrategy = .convertFromSnakeCase
         return decoder
     }()
     
@@ -104,4 +60,16 @@ final class UserManager {
     
     // more functions to update variables in DB will go here
     
+}
+
+// MARK: DBUser Factory Methods
+extension DBUser {
+    static func fromAuth(_ auth: AuthDataResultModel) -> DBUser {
+        DBUser(
+            userId: auth.uid,
+            email: auth.email,
+            photoUrl: auth.photoURL,
+            name: auth.name
+        )
+    }
 }
--- StepWorld/StepWorld/StepWorldApp.swift
+++ StepWorld/StepWorld/StepWorldApp.swift
@@ -17,16 +17,16 @@ struct StepWorldApp: App {
     var body: some Scene {
         WindowGroup {
             
-           /*
             ContentView()
                 .environmentObject(steps)
-            */
-           
+          
+            /*
             // for demo begin app in interactive map
             NavigationStack {
                 InteractiveGIFMapView()
                     .environmentObject(steps)
             }
+             */
         }
     }
     
