f3b06b8a5c74fd69667b66581bd8ffaa5b45c13a
Isai Soria <isaisoria@gmail.com>
2025-10-24 22:42:44 -0700

    feat: implement sign in view

--- StepWorld/StepWorld/Core/SignInView.swift
+++ StepWorld/StepWorld/Core/SignInView.swift
@@ -15,92 +15,98 @@ struct SignInView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
+            
+            ZStack {
                 
-                Text("StepWorld is Live!")
+                // Attaching background image
+                Image("SignInBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .allowsHitTesting(false)
                 
-                Spacer()
-                
-                /*
-                // following two buttons are for testing
-                // assign the email and password that will be used
-                Button("Set Email") {
-                    viewModel.email = "fakeUser@testing.com"
-                }
-                
-                Button("Set Password") {
-                    viewModel.password = "password"
-                }
-                */
-                TextField("Email...", text: $viewModel.email)
-                    .padding()
-                    .background(Color.gray.opacity(0.4))
-                    .cornerRadius(10)
-                
-                SecureField("Password...", text: $viewModel.password)
-                    .padding()
-                    .background(Color.gray.opacity(0.4))
-                    .cornerRadius(10)
-                                
-                                
-                
-                Spacer()
-                
-                // button to sign in user
-                Button{
-                    Task {
-                        do {
-                            try await viewModel.signIn()
-                            // typically you should be sending them to another view here
-                            isSignedIn = true
-                        } catch {
-                            print(error)
-                        }
-                       
-                    }
-                } label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(height: 55)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
+                VStack(spacing: 16) {
+                    
+                    // Title Text
+                    Text("Welcome To ")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .font(.custom("Press Start 2P", size: 25))
+                        .padding(.top, 60)
+                        .padding(.bottom, 10)
+                        .foregroundColor(Color(red: 0.180, green: 0.118, blue: 0.071))
+                        .shadow(color: .black.opacity(0.15), radius: 2, x: 0, y: 2)
+                    Text("StepWorld")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .font(.custom("Press Start 2P", size: 40))
+                        .padding(.top, 5)
+                        .padding(.bottom, 170)
+                        .foregroundColor(Color(red: 0.180, green: 0.118, blue: 0.071))
+                        .shadow(color: .black.opacity(0.15), radius: 2, x: 0, y: 2)
+                    
+                    // Make the space
+                    Spacer()
+                    
+                    // Email input
+                    TextField("Email...", text: $viewModel.email)
+                        .padding()
+                        .background(Color(red: 1.0, green: 0.9725, blue: 0.9059).opacity(0.75))
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(red: 0.9216, green: 0.8431, blue: 0.6980), lineWidth: 1)
+                        )
+                        .foregroundColor(Color(red: 0.2353, green: 0.1647, blue: 0.1176))
                         .cornerRadius(10)
+                        .frame(width: 300, height: 44)
+                    
+                    // Password input
+                    SecureField("Password...", text: $viewModel.password)
+                        .padding()
+                        .background(Color(red: 1.0, green: 0.9725, blue: 0.9059).opacity(0.75))
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(red: 0.9216, green: 0.8431, blue: 0.6980), lineWidth: 1)
+                        )
+                        .foregroundColor(Color(red: 0.2353, green: 0.1647, blue: 0.1176))
+                        .cornerRadius(10)
+                        .frame(width: 300, height: 44)
+                    
+                    // Space between text fields and button
+                    Spacer()
+                    
+                    // Button to sign in user
+                    Button{
+                        Task {
+                            do {
+                                try await viewModel.signIn()
+                                isSignedIn = true
+                            } catch {
+                                print(error)
+                            }
+                            
+                        }
+                    } label: {
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(Color(red: 1.0, green: 0.9725, blue: 0.9059))
+                            .frame(height: 55)
+                            .frame(maxWidth: 350)
+                            .cornerRadius(10)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red: 0.89, green: 0.49, blue: 0.30))
+                    .padding(.bottom, 50)
+                    
                 }
-                .navigationDestination(isPresented: $isSignedIn) {
-                    InteractiveGIFMapView()
-                }
+                .padding()
+                
             }
-            
-            
-            // link to profileView for debugging
-            NavigationLink(destination: ProfileView()) {
-                Text("Go To Profile")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+            // When bool value is changed it sends the user to MapView
+            .navigationDestination(isPresented: $isSignedIn) {
+                InteractiveGIFMapView()
             }
-            
-            NavigationLink(destination: MapView()) {
-                Text("Go To Map")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            
-            
         }
-            }
     }
+}
 
-    
-    #Preview {
-        SignInView()
-    }
+#Preview {
+    SignInView()
+}
 
