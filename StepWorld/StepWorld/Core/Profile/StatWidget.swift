9162ccbe53001cba8aa256c5a0fbfb99243b726b
Lebeis1 <andrelebeis3@gmail.com>
2025-10-26 20:07:36 -0700

    Added profile page

---
+++ StepWorld/StepWorld/Core/Profile/StatWidget.swift
@@ -0,0 +1,49 @@
+//
+//  StatWidget.swift
+//  StepWorld
+//
+//  Created by Andre Bortoloto Lebeis on 10/26/25.
+//
+
+import SwiftUI
+
+struct StatWidget: View {
+    let backgroundImageName: String
+    let title: String
+    let value: String
+    var icon: Image? = nil
+    var width: CGFloat = 280
+    var height: CGFloat = 95
+
+    var body: some View {
+        ZStack {
+            Image(backgroundImageName)
+                .interpolation(.none)
+                .antialiased(false)
+                .resizable()
+                .frame(width: width, height: height)
+
+            HStack {
+                VStack {
+                    
+                    Text(title)
+                        .font(.custom("Press Start 2P", size: 14))
+                    
+                    Text(value)
+                        .font(.custom("Press Start 2P", size: 14))
+                        .padding(.top, 5)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 140)
+        }
+    }
+}
+
+
+#Preview {
+    StatWidget(backgroundImageName: "CoinsWidget",
+               title: "Coins:",
+               value: "300")
+}
