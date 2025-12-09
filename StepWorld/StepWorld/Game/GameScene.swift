5b7212369e5cb940484dc7491e62c6a905f5d582
Isai Soria <isaisoria@gmail.com>
2025-12-08 20:33:57 -0800

    debug: fix issue with skins

--- StepWorld/StepWorld/Game/GameScene.swift
+++ StepWorld/StepWorld/Game/GameScene.swift
@@ -1472,6 +1472,16 @@ final class GameScene: SKScene {
             attachForSaleSign(to: plot, plotSize: size)
         }
     }
+    
+    // MARK: - Server skin state bridge
+    func applySkinStateFromServer(owned: Set<String>, equipped: [String:String]) {
+        ownedSkins = owned
+        equippedSkinForType = equipped
+
+        for (base, skin) in equipped {
+            applySkinToAllBuildings(of: base, skin: skin)
+        }
+    }
 
 
     // MARK: - MapManager helpers
